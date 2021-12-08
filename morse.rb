bbbab9ef67c21a89ae8da5d4807c6c0bab97f934
Hichami Houssam <hichami.houssam@gmail.com>
2021-12-08 16:35:21 +0100

    fixing linter errors

--- morse.rb
+++ morse.rb
@@ -31,20 +31,17 @@ def decode_char(str)
   @morse_dict[str].upcase
 end
 
-
-
 def decode_word(word)
   letters = word.split.map { |str| decode_char(str) }
   letters.join
 end
-
 
 def decode_message(message)
   words = message.split('   ').map { |word| decode_word(word) }
   words.join(' ')
 end
 
-puts "Enter a valid morse message : "
+puts 'Enter a valid morse message : '
 message = gets.chomp
 puts "this morse message translates to  #{decode_message(message)}"
 # A BOX FULL OF RUBIES
